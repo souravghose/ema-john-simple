c487320c6d9657b3f8ae2ee174fe9e5716aac8eb
Sourav Ghose <sourav.ghose10@gmail.com>
2023-10-29 21:14:21 +0600

    shop product showing done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/Header'
+import Shop from './components/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
         <Header></Header>
+        <Shop></Shop>
       </div>
     </>
   )
