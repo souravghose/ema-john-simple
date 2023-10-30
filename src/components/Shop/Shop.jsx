67af22587c1781854a7617c51f1dc1cb579daa34
Sourav Ghose <sourav.ghose10@gmail.com>
2023-10-30 12:29:18 +0600

    shopping cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -10,17 +10,25 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
+
+    const [cart, setCart] = useState([])
+    const handleAddtoCart = (product) => {
+       const newCart = [...cart, product]
+       setCart(newCart)
+    }
     return (
       <div className="shop-container">
         <div className="product-container">
            {products.map(product => <Product 
            key={product.id}
-           product={product}>
-          
+           product={product}
+           handleAddtoCart={handleAddtoCart}>
+           
            </Product>)}
         </div>
         <div className="order-container">
             <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
         </div>
       </div>
     );
